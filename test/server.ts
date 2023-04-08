3e94b832d7dc7bccbab36ce25b1f0a12d7f4b0ef
gnarus-g <37311893+Gnarus-G@users.noreply.github.com>
2023-04-08 15:55:33 -0400

    feat: info logs in deno server

--- test/server.ts
+++ test/server.ts
@@ -3,8 +3,17 @@ import express from "npm:express@4.18.2";
 
 const app = express();
 
+app.use((_, __, next) => {
+  console.log("request at", new Date());
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send(`Welcome to the Dinosaur API! ${req.header("random")}`);
 });
 
-app.listen(8080);
+const PORT = 8080;
+
+console.log("Listening on port:", PORT);
+
+app.listen(PORT);
