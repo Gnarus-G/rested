7285ba222ac7c1b8afa41b852d0fd10539899279
gnarus-g <37311893+Gnarus-G@users.noreply.github.com>
2023-05-13 10:04:07 -0400

    fix: revived the parser tests with insta

--- parser/tests/parser.rs
+++ parser/tests/parser.rs
@@ -2,9 +2,23 @@ use insta::assert_ron_snapshot;
 
 use parser::Parser;
 
+macro_rules! assert_ast {
+    ($input:literal) => {
+        let mut parser = Parser::new($input);
+        let ast = parser.parse().unwrap();
+
+        insta::with_settings!({
+             description => $input
+        }, {
+            assert_ron_snapshot!(ast);
+        })
+    };
+}
+
 #[test]
 fn it_works() {
-    let code = r#"
+    assert_ast!(
+        r#"
 set BASE_URL "httas..."
 post http://lasdf.. {}
 // asdfasdf
@@ -28,12 +42,97 @@ get /api {}
 put /api {}
 patch /api {}
 delete /api {}
-"#;
+"#
+    );
+}
 
-    let p = Parser::new(code).parse().unwrap();
-    insta::with_settings!({
-         description => code
-    }, {
-        assert_ron_snapshot!(p);
-    })
+use lexer::locations::Location;
+
+pub fn at(line: usize, col: usize) -> Location {
+    Location { line, col }
+}
+
+#[test]
+fn parse_get_urls() {
+    assert_ast!(
+        r#"get http://localhost:8080
+        get http://localhost:8080 {}"#
+    );
+}
+
+#[test]
+fn parse_post_url() {
+    assert_ast!("post http://localhost");
+
+    assert_ast!("post /api/v2");
+}
+
+#[test]
+fn parse_attributes() {
+    assert_ast!(r#"@log("path/to/file") get /api"#);
+}
+
+#[test]
+fn parse_get_with_headers() {
+    assert_ast!(
+        r#"
+        get http://localhost {
+        header "Authorization" "Bearer token"
+        header "random" "tokener Bear"
+        }"#
+    );
+}
+
+#[test]
+fn parse_post_with_headers() {
+    assert_ast!(
+        r#"
+        post http://localhost {
+        header "Authorization" "Bearer token"
+        header "random" "tokener Bear"
+        }"#
+    );
+}
+#[test]
+fn parse_post_with_headers_and_body() {
+    assert_ast!(
+        r#"
+        post http://localhost {
+        header "Authorization" "Bearer token"
+        body "{neet: 1337}"
+        }"#
+    );
+}
+
+#[test]
+fn parse_post_with_headers_and_body_as_json_string() {
+    assert_ast!(
+        r#"
+        post http://localhost {
+        header "Authorization" "Bearer token"
+        body `
+        {"neet": 1337}
+        `
+        }"#
+    );
+}
+
+#[test]
+fn parse_env_call_expression() {
+    assert_ast!(r#"post http://localhost { header "name" env("auth") body env("data") }"#);
+}
+
+#[test]
+fn parse_global_constant_setting() {
+    assert_ast!("set BASE_URL \"stuff\"");
+}
+
+#[test]
+fn parse_template_string_literal() {
+    assert_ast!(
+        r#"
+        post /api {
+            body `{"neet": ${env("love")}, 2: ${"two"}}`
+        }"#
+    );
 }
