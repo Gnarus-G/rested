ec675e6a8346d1a98cfd5c1ad52901e913866e34
gnarus-g <37311893+Gnarus-G@users.noreply.github.com>
2023-05-13 19:29:24 -0400

    feat: very basic lsp completions

--- language_server/src/lib.rs
+++ language_server/src/lib.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 use std::sync::Mutex;
 
-use lexer::locations::Location;
+use lexer::locations::{Location, Span};
 use parser::Parser;
 use tower_lsp::jsonrpc::Result;
 use tower_lsp::{lsp_types::*, LspService, Server};
@@ -90,6 +90,19 @@ impl Backend {
     }
 }
 
+trait ContainsPosition {
+    fn contains(&self, position: &Position) -> bool;
+}
+
+impl ContainsPosition for Span {
+    fn contains(&self, position: &Position) -> bool {
+        if self.start.line == self.end.line {
+            return (self.start.col..=self.end.col).contains(&(position.character as usize));
+        }
+        (self.start.line..=self.end.line).contains(&(position.line as usize))
+    }
+}
+
 #[tower_lsp::async_trait]
 impl LanguageServer for Backend {
     async fn initialize(&self, _: InitializeParams) -> Result<InitializeResult> {
@@ -99,6 +112,9 @@ impl LanguageServer for Backend {
                 text_document_sync: Some(TextDocumentSyncCapability::Kind(
                     TextDocumentSyncKind::FULL,
                 )),
+                completion_provider: Some(CompletionOptions {
+                    ..CompletionOptions::default()
+                }),
                 hover_provider: Some(HoverProviderCapability::Simple(true)),
                 ..ServerCapabilities::default()
             },
@@ -122,6 +138,83 @@ impl LanguageServer for Backend {
             text: params.text_document.text,
         })
         .await;
+    }
+
+    async fn completion(&self, params: CompletionParams) -> Result<Option<CompletionResponse>> {
+        let position = params.text_document_position.position;
+        let Some(text) = self
+            .documents
+            .get(params.text_document_position.text_document.uri)
+            else {
+                return Ok(None);
+            };
+
+        let methods = ["get", "post", "put", "patch", "delete"]
+            .map(|keyword| CompletionItem {
+                label: format!("{}", keyword),
+                kind: Some(CompletionItemKind::KEYWORD),
+                text_edit: Some(CompletionTextEdit::Edit(TextEdit {
+                    new_text: format!("{}", keyword),
+                    range: Range::new(
+                        position,
+                        Position {
+                            line: position.line,
+                            character: keyword.len() as u32,
+                        },
+                    ),
+                })),
+                ..CompletionItem::default()
+            })
+            .to_vec();
+
+        let functions = ["env", "read", "escape_new_lines"]
+            .map(|keyword| CompletionItem {
+                label: format!("{}(..)", keyword),
+                kind: Some(CompletionItemKind::FUNCTION),
+                text_edit: Some(CompletionTextEdit::Edit(TextEdit {
+                    new_text: format!("{}(", keyword),
+                    range: Range::new(
+                        position,
+                        Position {
+                            line: position.line,
+                            character: keyword.len() as u32,
+                        },
+                    ),
+                })),
+                ..CompletionItem::default()
+            })
+            .to_vec();
+
+        let Ok(ast) = parser::Parser::new(&text).parse() else {
+            return Ok(Some(CompletionResponse::Array(functions)));
+        };
+
+        for item in ast.items {
+            if let parser::ast::Item::Request { block, .. } = item {
+                if let Some(block) = block {
+                    let contains_position = block.span.contains(&position);
+                    if contains_position {
+                        let statement_types = ["header", "body"]
+                            .map(|kw| kw.to_string())
+                            .map(|keyword| CompletionItem {
+                                label: keyword.clone(),
+                                kind: Some(CompletionItemKind::KEYWORD),
+                                insert_text: Some(keyword),
+                                ..CompletionItem::default()
+                            })
+                            .to_vec();
+
+                        return Ok(Some(CompletionResponse::Array(
+                            [statement_types, functions].concat(),
+                        )));
+                    }
+                }
+            }
+        }
+
+        return Ok(Some(CompletionResponse::Array(
+            [methods, functions].concat(),
+        )));
     }
 
     async fn did_change(&self, params: DidChangeTextDocumentParams) {
