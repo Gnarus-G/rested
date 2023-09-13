4a4bd1116b9fa1c45a52a51ddf908f10e3071258
gnarus-g <37311893+Gnarus-G@users.noreply.github.com>
2023-09-12 23:47:14 -0400

    fix: lint

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ use clap::{CommandFactory, Parser, Subcommand};
 use cli::config::ConfigArgs;
 use cli::run::RunArgs;
 use cli::scratch::ScratchCommandArgs;
-use rested::config::Config;
 use rested::editing::edit;
 use rested::interpreter::environment::Environment;
 
