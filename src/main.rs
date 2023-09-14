e65c7b049b88997a7ebbccd7c3a9b25b25c3756e
gnarus-g <37311893+Gnarus-G@users.noreply.github.com>
2023-09-13 23:26:40 -0400

    feat: prefer info for default log level

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ struct Cli {
     command: Command,
 
     /// Set log level
-    #[arg(short, long, default_value = "debug", global = true)]
+    #[arg(short, long, default_value = "info", global = true)]
     level: tracing::Level,
 }
 
