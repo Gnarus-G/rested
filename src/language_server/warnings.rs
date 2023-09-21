9007e0b8a1c7ec737cff6acd47258764e59a92fe
gnarus-g <37311893+Gnarus-G@users.noreply.github.com>
2023-09-20 22:53:05 -0400

    feat: applying visitor pattern to implement some warnings for env vars that aren't defined for all namespaces

---
+++ src/language_server/warnings.rs
@@ -0,0 +1,67 @@
+use crate::{
+    interpreter,
+    lexer::Token,
+    parser::{
+        ast::{self, Expression},
+        ast_visit::{self},
+    },
+};
+use tower_lsp::lsp_types::*;
+
+use super::IntoPosition;
+
+pub struct EnvVarsNotInAllNamespaces<'env> {
+    pub env: &'env interpreter::environment::Environment,
+    pub warnings: Vec<tower_lsp::lsp_types::Diagnostic>,
+}
+
+impl<'env> EnvVarsNotInAllNamespaces<'env> {
+    pub fn new(env: &'env interpreter::environment::Environment) -> Self {
+        Self {
+            env,
+            warnings: vec![],
+        }
+    }
+}
+
+impl<'env> ast_visit::Visitor for EnvVarsNotInAllNamespaces<'env> {
+    fn visit_expr(&mut self, expr: &Expression) {
+        if let Expression::Call {
+            arguments,
+            identifier: ast::result::ParsedNode::Ok(Token { text: "env", .. }),
+        } = expr
+        {
+            for arg in &arguments.parameters {
+                match arg {
+                    Expression::String(value) => {
+                        let namespaces_from_which_var_is_missing = self
+                            .env
+                            .namespaced_variables
+                            .iter()
+                            .filter(|(_, vars)| !vars.contains_key(&value.value.to_string()))
+                            .map(|(namespace, _)| namespace)
+                            .cloned()
+                            .collect::<Vec<_>>();
+
+                        if !namespaces_from_which_var_is_missing.is_empty() {
+                            self.warnings.push(Diagnostic {
+                                range: Range {
+                                    start: value.span.start.into_position(),
+                                    end: value.span.end.into_position(),
+                                },
+                                message: format!(
+                                    "variable '{}' missing from some namespaces: {}",
+                                    value.value,
+                                    namespaces_from_which_var_is_missing.join(", ")
+                                ),
+                                severity: Some(DiagnosticSeverity::WARNING),
+                                ..Default::default()
+                            })
+                        }
+                    }
+                    _ => self.visit_expr(expr),
+                }
+            }
+        }
+    }
+}
